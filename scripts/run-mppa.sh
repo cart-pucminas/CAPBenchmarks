188e77a44fd9ce80a4c622f0286dd7c285512dc5
Marcio Castro <mbcastro@gmail.com>
2016-01-13 19:46:19 +0100

    Adding all kernels to the run-mppa.sh script.

--- scripts/run-mppa.sh
+++ scripts/run-mppa.sh
@@ -15,8 +15,7 @@ export CLASS=standard
 mkdir -p $RESULTSDIR
 
 echo "Problem size = $CLASS"
-#for kernel in fast fn gf is km lu tsp; do
-for kernel in tsp; do	
+for kernel in fast fn gf is km lu tsp; do
 	for nprocs in {1..16}; do
 	    echo "  >> running $kernel with $nprocs cluster(s)."
 	    $MPPADIR/bin/k1-power -- $MPPADIR/bin/k1-jtag-runner --multibinary=$BINDIR/$kernel.img --exec-multibin=IODDR0:master -- --verbose --class $CLASS --nclusters $nprocs &>> $RESULTSDIR/$kernel-standard-$nprocs.mppa
