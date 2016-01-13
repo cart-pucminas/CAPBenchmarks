19c6a2482f6229b902585319d5b4a13c8817fd72
Marcio Castro <mbcastro@gmail.com>
2016-01-13 19:17:57 +0100

    run-mppa.sh script updated.

--- scripts/run-mppa.sh
+++ scripts/run-mppa.sh
@@ -8,27 +8,17 @@ export RESULTSDIR=$ROOTDIR/results
 export BINDIR=$ROOTDIR/bin
 export MPPADIR=/usr/local/k1tools
 
+# Problem size.
+export CLASS=standard
+
 # Create results directory.
 mkdir -p $RESULTSDIR
 
-for kernel in fast fn gf is km lu tsp; do
-	echo "running $kernel"
-	
-	# Weak scaling
-	echo "running weak scaling test: iteration $it"
-	for nprocs in 1 2 4 8 16; do
-		for class in tiny small large huge; do
-			$MPPADIR/bin/k1-power -- $MPPADIR/bin/k1-jtag-runner --multibinary=$BINDIR/$kernel.mppa.mpk --exec-multibin=IODDR0:$kernel.master -- --verbose --class $class --nclusters $nprocs &>> $RESULTSDIR/$kernel-$class-$nprocs.mppa
-		done
-	done
-	
-	# Strong scaling.
-	for it in {1..10}; do
-		echo "running strong scaling test: iteration $it"
-		for nprocs in 1 2 3 5 6 7 8 9 10 11 12 13 14 15 16; do
-			$MPPADIR/bin/k1-power -- $MPPADIR/bin/k1-jtag-runner --multibinary=$BINDIR/$kernel.mppa.mpk --exec-multibin=IODDR0:$kernel.master -- --verbose --class standard --nclusters $nprocs &>> $RESULTSDIR/$kernel-standard-$nprocs.mppa
-		done
+echo "Problem size = $CLASS"
+#for kernel in fast fn gf is km lu tsp; do
+for kernel in tsp; do	
+	for nprocs in {1..16}; do
+	    echo "  >> running $kernel with $nprocs cluster(s)."
+	    $MPPADIR/bin/k1-power -- $MPPADIR/bin/k1-jtag-runner --multibinary=$BINDIR/$kernel.img --exec-multibin=IODDR0:master -- --verbose --class $CLASS --nclusters $nprocs &>> $RESULTSDIR/$kernel-standard-$nprocs.mppa
 	done
 done
-
-
