6982b3e6154ceb5a5c1b941922c4687dcbc0aa60
DavidGrunheidt <davidordine@hotmail.com>
2018-12-26 13:54:07 -0200

    Fixing KM x86 kernel

--- x86/src/KM/main.c
+++ x86/src/KM/main.c
@@ -1,52 +1,34 @@
-/*
- * Copyright(C) 2014 Pedro H. Penna <pedrohenriquepenna@gmail.com>
- * 
- * Kmeans Benchmark Kernel.
- */
-
-#include <omp.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <string.h>
+/* Kernel Include */
 #include <timer.h>
 #include <util.h>
 #include "km.h"
 
-/*
- * Problem.
- */
-struct problem
-{
+/* C And MPPA Library Includes*/
+#include <omp.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+/* Problem. */
+struct problem {
 	int npoints;       /* Number of points.    */
-	int dimension;     /* Data dimension.      */
 	int ncentroids;    /* Number of centroids. */
-	float mindistance; /* Minimum distance.    */
 };
 
 /* Problem sizes. */
-static struct problem tiny     = {  4096, 16,  256, 0.0 };
-static struct problem small    = {  8192, 16,  512, 0.0 };
-static struct problem standard = { 16384, 16, 1024, 0.0 };
-static struct problem large    = { 32768, 16, 1024, 0.0 };
-static struct problem huge     = { 65536, 16, 1024, 0.0 };
+static struct problem tiny     = {  4096, 256};
+static struct problem small    = {  8192, 512};
+static struct problem standard = { 16384, 1024};
+static struct problem large    = { 32768, 1024};
+static struct problem huge     = { 65536, 1024};
 
-/* Be verbose? */
-int verbose = 0;
+static int seed = 0;              /* Seed number. */      
+static struct problem *p = &tiny; /* Problem. */   
+int verbose = 0;                  /* Be verbose? */            
+int nthreads = 1;                 /* Number of threads. */       
 
-/* Number of threads. */                
-int nthreads = 1;
-
-/* Seed number. */
-static int seed = 0;
-
-/* Problem. */           
-static struct problem *p = &tiny;
-
-/*
- * Prints program usage and exits.
- */
-static void usage(void)
-{
+/* Prints program usage and exits. */
+static void usage() {
 	printf("Usage: kmeans [options]\n");
 	printf("Brief: Kmeans Benchmark Kernel\n");
 	printf("Options:\n");
@@ -61,11 +43,8 @@ static void usage(void)
 	exit(0);
 }
 
-/*
- * Reads command line arguments.
- */
-static void readargs(int argc, char **argv)
-{
+/* Reads command line arguments. */
+static void readargs(int argc, char **argv) {
 	int i;     /* Loop index.       */
 	char *arg; /* Working argument. */
 	int state; /* Processing state. */
@@ -133,56 +112,54 @@ static void readargs(int argc, char **argv)
 		usage();
 }
 
-/*
- * Runs benchmark.
- */
-int main(int argc, char **argv)
-{
-	int i;          /* Loop index.      */
-	int *map;       /* Map of clusters. */
-	uint64_t end;   /* End time.        */
-	uint64_t start; /* Start time.      */
-	vector_t *data; /* Data points.     */
-	
+/* Runs benchmark. */
+int main(int argc, char **argv) {
+	int i;               /* Loop index.             */
+	int *map;            /* Map of clusters.        */
+	int dimension;       /* Problem dimension.      */
+	float *points;       /* Points being clustered. */
+	uint64_t start, end; /* Start and End time.     */
+
 #ifdef _XEON_PHI_
 	double power;
 #endif
-	
+
 	readargs(argc, argv);
-	
+
 	timer_init();
 	srandnum(seed);
 	omp_set_num_threads(nthreads);
-	
+
+	/* Setting the dimension for the problem. */
+	dimension = 16; 
+
 	/* Benchmark initialization. */
 	if (verbose)
 		printf("initializing...\n");
 	start = timer_get();
-	data = smalloc(p->npoints*sizeof(vector_t));
-	for (i = 0; i < p->npoints; i++)
-	{
-		data[i] = vector_create(p->dimension);
-		vector_random(data[i]);
-	}
+	points = smalloc(p->npoints*dimension*sizeof(float));
+	for (i = 0; i < p->npoints * dimension; i++)
+		points[i] = randnum() & 0xffff;
 	end = timer_get();
+
 	if (verbose)
 		printf("  time spent: %f\n", timer_diff(start, end)*MICROSEC);
-	
+
 #ifdef _XEON_PHI_
 	power_init();
 #endif
-	
+
 	/* Cluster data. */
 	if (verbose)
 		printf("clustering data...\n");
 	start = timer_get();
-	map = kmeans(data, p->npoints, p->ncentroids, p->mindistance);
+	map = kmeans(points, p->npoints, p->ncentroids, dimension);
 	end = timer_get();
-	
+
 #ifdef _XEON_PHI_
 	power = power_end();
 #endif
-	
+
 	printf("timing statistics:\n");
 	printf("  total time:    %f\n", timer_diff(start, end)*MICROSEC);
 
@@ -192,9 +169,8 @@ int main(int argc, char **argv)
 	
 	/* House keeping. */
 	free(map);
-	for (i = 0; i < p->npoints; i++)
-		vector_destroy(data[i]);
-	free(data);
+	free(points);
 	
 	return (0);
+
 }
