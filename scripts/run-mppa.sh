f8abc93bce07296fd8553fe3fb4b46b70a0b9edf
Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
2020-08-04 15:52:36 -0300

    [scripts] Enhancement: Simpler Run Script for MPPA-256

--- scripts/run-mppa.sh
+++ scripts/run-mppa.sh
@@ -6,20 +6,15 @@
 export BINDIR=bin
 export K1DIR=/usr/local/k1tools/bin
 
-echo Inform class size and number of clusters:
-read classize nclusters
-
 # Default Parameters.
-export CLASS=$classize
-export NPROCS=$nclusters
+export KERNEL=$1
+export CLASS=$2
+export NPROCS=$3
 
 echo "Problem size = $CLASS"
 
-for kernel in tsp; #fn lu km gf fast is tsp;
-do
-	echo "  ========== Running $kernel kernel"
-	$K1DIR/k1-jtag-runner                        		\
-		--multibinary=$BINDIR/$kernel.img               \
-		--exec-multibin=IODDR0:io_bin                   \
-		-- --verbose --class $CLASS --nclusters $NPROCS
-done
+echo "  ========== Running $KERNEL kernel"
+$K1DIR/k1-jtag-runner                        		\
+	--multibinary=$BINDIR/$KERNEL.img               \
+	--exec-multibin=IODDR0:io_bin                   \
+	-- --verbose --class $CLASS --nclusters $NPROCS
